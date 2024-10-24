deaf31e889ea461daf803960223613cd35dee723
Bruno H. Homem <brunohhomem@gmail.com>
2024-10-24 18:27:05 -0300

    feat: create UserRepository

---
+++ src/main/java/com/brunohhomem/picpay_desafio/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.brunohhomem.picpay_desafio.repositories;
+
+import com.brunohhomem.picpay_desafio.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findUserByDocument(String document);
+
+    Optional<User> findUserById(Long id);
+}

fb85c6a9537e079f27142c228ac71d55e1279560
Bruno H. Homem <brunohhomem@gmail.com>
2024-10-24 18:05:16 -0300

    first commit

---
+++ src/test/java/com/brunohhomem/picpay_desafio/PicpayDesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brunohhomem.picpay_desafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicpayDesafioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

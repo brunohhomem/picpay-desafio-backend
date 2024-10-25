fb2cd9f8b977370c9b5cc81c178133e36e85a4a0
Bruno H. Homem <brunohhomem@gmail.com>
2024-10-25 06:56:40 -0300

    feat: create UserService

---
+++ src/main/java/com/brunohhomem/picpay_desafio/services/UserService.java
@@ -0,0 +1,35 @@
+package com.brunohhomem.picpay_desafio.services;
+
+import com.brunohhomem.picpay_desafio.domain.user.User;
+import com.brunohhomem.picpay_desafio.domain.user.UserType;
+import com.brunohhomem.picpay_desafio.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public void validateTransaction(User sender, BigDecimal amount) throws Exception {
+        if (sender.getUserType() == UserType.MERCHANT) {
+            throw new Exception("Usuário do tipo lojista não está autorizado a realizar autorização");
+        }
+
+        if (sender.getBalance().compareTo(amount) < 0) {
+            throw new Exception("Saldo insuficiente.");
+        }
+    }
+
+    public User findUserById(Long id) throws Exception {
+        return this.userRepository.findUserById(id).orElseThrow(() -> new Exception("Usuário não encontrado"));
+
+    }
+
+    public void saveUser(User user) {
+        this.userRepository.save(user);
+    }
+}
